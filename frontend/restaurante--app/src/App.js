cdb3c6b0ee3d1a8bf0bd7f05344c01debcc08630
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-15 21:36:05 -0300

    parte 6

--- frontend/restaurante--app/src/App.js
+++ frontend/restaurante--app/src/App.js
@@ -5,9 +5,12 @@ import Rodape from './Rodape/Rodape'
 export default function App() {
 
       return (
-        <div >
+        <div className="app-1">
             <Cabecalho />
-            <div className="app-1">Oi React! Aplicação em Restaurante</div>
+            <div className="app-2">
+                <div >Restaurante XXX</div>
+                <div >Oi React! Aplicação em Restaurante</div>
+            </div>
             <Rodape />
         </div>
       )
--- frontend/restaurante--app/src/Cabecalho/Cabecalho.js
+++ frontend/restaurante--app/src/Cabecalho/Cabecalho.js
@@ -8,7 +8,16 @@ export default function Cabecalho() {
 
       return (
           <header className="cabecalho_base">
-                <div className="cabecalho_inf1">meu cabeçalho</div>
+                <div className="cabecalho_inf1">
+                        <div className="cabecalho_inf2">meu cabeçalho</div>
+                        <div className="cabecalho_inf2">meu cabeçalho</div>
+                        <div className="cabecalho_inf2">meu cabeçalho</div>
+                        <div className="cabecalho_inf2">meu cabeçalho</div>
+                        <div className="cabecalho_inf2">meu cabeçalho</div>
+                        <div className="cabecalho_inf2">meu cabeçalho</div>
+
+                </div>
+                
                   
           </header>
       )
