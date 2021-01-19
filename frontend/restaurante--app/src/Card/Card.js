2305ba2823de71e6149a06629631d88ef711f1eb
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-18 21:53:07 -0300

    parte 13

--- frontend/restaurante--app/src/Card/Card.js
+++ frontend/restaurante--app/src/Card/Card.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import './Card.css'
-
+import sardinha from '../images/sardinha.jpg'
 
 
 export default function Card() {
 
 
       return (
-          <div className="card--base">Isto é um card</div>
+          <div className="card--base">
+              <img src={sardinha} alt="sardinha" className="imagem" />
+              <div>Nome</div>
+              <div>Preço</div>
+              <div>Descrição</div>
+              <div>
+
+              </div>
+              <button>Adicionar</button>
+          </div>
       )
 }
 
