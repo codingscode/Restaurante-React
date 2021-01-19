c0af4346d010843de5f6dfc33074db705c114c25
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-18 21:28:11 -0300

    parte 12

--- frontend/restaurante--app/src/App.js
+++ frontend/restaurante--app/src/App.js
@@ -1,6 +1,8 @@
 import './App.css'
 import Cabecalho from './Cabecalho/Cabecalho'
 import Rodape from './Rodape/Rodape'
+import Card from './Card/Card'
+
 
 export default function App() {
 
@@ -12,59 +14,29 @@ export default function App() {
             <div className="painel--geral_meio">
                 <div className="titulo">Restaurante Panela de Barro</div>
                 <div className="grid-container">
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
-                    <div>Aqui é o conteúdo</div>
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                                        
                 </div>
-                <div></div>
+                
             </div>
             <Rodape />
         </div>
---
+++ frontend/restaurante--app/src/Card/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Card.css'
+
+
+
+export default function Card() {
+
+
+      return (
+          <div className="card--base">Isto é um card</div>
+      )
+}
+
