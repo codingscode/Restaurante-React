81f2c1b36706b4b84d02a01e7760385a2e3af3fb
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-19 18:46:59 -0300

    parte 16

--- frontend/restaurante--app/src/Card/Card.js
+++ frontend/restaurante--app/src/Card/Card.js
@@ -6,26 +6,35 @@ import './Card.css'
 
 export default function Card() {
 
+      const [quantidade, setQuantidade] = useState(0)
+      
+      let imagem1 = 'sardinha.jpg'
+      
       
 
-      
-
-      let valor = 'vatapa.jpg'
-      let valor2 = 'feijoada.jpg'
-     
+      function valorQuantidade(operacao) {
+          
+          if (operacao === '+') {
+              setQuantidade((q) => q += 1)
+          }
+          if (operacao === '-' && quantidade > 0) {
+              setQuantidade((q) => q -= 1)
+          }
+          
+      }     
       
 
       return (
           <div className="card--base">
-              <img src="./images/sardinha.jpg" alt="sardinha" className="imagem" />
-              <img src={`./images/${valor}`} alt="sardinha" className="imagem" />
-              <img src={`./images/${valor2}`} alt="sardinha" className="imagem" />
-              
-              <div>Nome</div>
-              <div>Preço</div>
-              <div>Descrição</div>
-              <div>
-
+              <img src={`./images/${imagem1}`} alt="sardinha" className="imagem" />
+              <div>Nome: </div>
+              <div>Preço: </div>
+              <div>Descrição: </div>
+              <div className="quantidade">
+                   Quantidade: <span>{quantidade}</span>
+                  
+                   <button onClick={() => valorQuantidade('-')} >-</button>
+                   <button onClick={() => valorQuantidade('+')} >+</button>
               </div>
               <button>Adicionar</button>
           </div>
